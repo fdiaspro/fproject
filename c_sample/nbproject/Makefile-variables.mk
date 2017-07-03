35244fddd125fd216ce93c400d4cc093b5cc52cc
fdiaspro <Admin@Toshiba-AL>
2017-07-03 09:41:47 +0200

    prova di invio

--- c_sample/nbproject/Makefile-variables.mk
+++ c_sample/nbproject/Makefile-variables.mk
@@ -15,13 +15,13 @@ CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
 CND_PACKAGE_NAME_Debug=csample.tar
 CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/csample.tar
 # Release configuration
-CND_PLATFORM_Release=GNU-Linux
-CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
+CND_PLATFORM_Release=None-Linux
+CND_ARTIFACT_DIR_Release=dist/Release/None-Linux
 CND_ARTIFACT_NAME_Release=c_sample
-CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/c_sample
-CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
+CND_ARTIFACT_PATH_Release=dist/Release/None-Linux/c_sample
+CND_PACKAGE_DIR_Release=dist/Release/None-Linux/package
 CND_PACKAGE_NAME_Release=csample.tar
-CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/csample.tar
+CND_PACKAGE_PATH_Release=dist/Release/None-Linux/package/csample.tar
 #
 # include compiler specific variables
 #
