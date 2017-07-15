204cffe2b23d7a2fd30e2fd1b370951ba9af3cb7
fdiaspro <Admin@Toshiba-AL>
2017-07-15 22:35:27 +0200

    prova di invio

--- prova_for/main.cpp
+++ prova_for/main.cpp
@@ -54,11 +54,89 @@ void func()
 }
     
 
+
+int func4(void) { 
+      string str1, str2; 
+      cout << "Enter 2 lines of text:" << endl; 
+	getline(cin,str1); 
+	getline(cin,str2); 
+	cout << "You've entered:'" << endl; 
+	if(str1 == str2) 
+		cout << "\"" << str1 << "\" == \"" << str2 << "\"" << endl; 
+	else if(str1 > str2) 
+		cout << "\"" << str1 << "\" > \"" << str2 << "\"" << endl; 
+	else 
+		cout << "\"" << str2 << "\" > \"" << str1 << "\"" << endl; 
+      return 0; 
+}
+
+
+void printInfo(string &s) { 
+        cout << "length = " << s.length() << endl; 
+        cout << "capacity = " << s.capacity() << endl; 
+        cout << "max size = " << s.max_size() << endl; 
+        cout << "---------" << endl; 
+} 
+
+int func5(void) { 
+        string TheString = "content"; 
+        printInfo(TheString); 
+        for(int i = 0; i < 10; i++) 
+                TheString += TheString; 
+        printInfo(TheString); 
+        return 0; 
+} 
+int func6() {
+	string S = "ABC"; 
+	cout << S.compare(1,1,"BC") << S.compare(2,1,S,2,2) << endl;
+	return 0;
+}
+
+
+
+class base {
+    string S;
+public:
+      void print() { std::cout << "base" << std::endl ;} ;
+};
+
+class derived :public base {
+    string S;
+public:
+      void print() { std::cout << "derived" << std::endl ;} ;
+};
+
+
+   string fun7(string &t, string s = "", int r = 2) {
+        while(--r)
+            s += s;
+        t = t + s;
+        return s;
+    }
+    
+    int fun8() {
+        string name = "x";
+        cout << fun7(name, name);
+        cout << name << endl;
+        return 0;
+    }
+    
+ 
+
+
 int main(void) {
     int pow = 1;
-    
-    func3();
-    
+        string s1 = "1";
+        string s2 = "12";
+        
+        base* b= new derived;
+        derived* d = static_cast<derived*> ( new base );
+        b->print();
+        d->print();
+        
+        cout << endl<<s1.compare(s2)<<endl;
+        fun8();
+    return 0;
     for(int exp = 0; exp < 16; exp++) {
         cout << "2 to the power of " << exp << " is " << pow << endl;
         pow *= 2;
